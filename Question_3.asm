993f2f50937490173346852bb60f2b14f53ff4e6
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-06-10 15:16:24 -0300

    Create Question_3.asm

---
+++ Question_3.asm
@@ -0,0 +1,62 @@
+org 0x7c00
+jmp _start
+
+_start:
+    xor ax, ax
+    
+    call getchar
+    call endl
+
+    call getchar
+    call endl
+
+    call getchar
+    call endl
+    
+    call getchar
+    call endl
+    
+    call teste
+    call done
+    
+getchar:
+    mov ah, 0x00 
+    int 16h
+    call putchar
+    
+    sub ax, '0'
+    
+    stosb
+    ret
+    
+putchar:
+    mov ah, 0x0e
+    int 10h
+    ret
+    
+endl:
+    mov ah, 0x0a
+    call putchar
+    mov ah, 0x0d
+    call putchar
+    ret
+    
+teste:
+    lodsb
+    cmp al, 0
+    je .done
+    add ax, '0'
+    call putchar
+    call endl
+    jmp teste
+    
+    .done:
+        ret
+    
+done:
+    jmp $
+    
+times 510 - ($ - $$) db 0
+dw 0xaa55
+
+
