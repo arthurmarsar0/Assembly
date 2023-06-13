1801a67fff5e13a5141b7f71eb8c2c66570e94c6
Mend25 <113634496+Mend25@users.noreply.github.com>
2023-06-13 17:41:22 -0300

    Update Question_6.asm

--- Question_6.asm
+++ Question_6.asm
@@ -2,70 +2,72 @@ org 0x7c00
 jmp _start
 
 _start:
-    xor ax,ax
-    xor si, si
+    xor ax, ax
     xor cx, cx
-    xor dx, dx
+    xor si, si
+    mov ds, ax
     
+    mov ah, 0
+    mov al, 12h
+    int 10h
+
+    xor ax, ax
+    xor cx, cx
+    xor si, si
+
+
+    mov bl, 0xf
     call getinput
     call endl
-    
+
     call solve
-    
-    ;xor cx, cx
-    ;xor bx, bx
-    ;mov bx, 1
-    ;push bx
-    ;push bx
-    ;call fib
-    
-    call done
-    
+
+    jmp done
+
 getinput:
-    mov ah, 0x00 
+    mov ah, 0x0
     int 16h
-    
+
     cmp al, 0x0d
-    jmp .done
-    
+    je .done
+
     inc cx
-    
-    sub ax, '0' 
+
+    sub ax, '0'
     stosb
     add ax, '0'
-    
-    call putchar
-    
-    jmp getinput
-    
-    .done: 
-        ret
-        
-putchar:
-    mov ah, 0x0e
-    int 10h
-    ret
 
+    call putchar
+
+    jmp getinput
+
+    .done:
+        ret
 endl:
     mov ax, 0x0a
     call putchar
     mov ax, 0x0d
     call putchar
     ret
-    
+
+putchar:
+    mov ah, 0x0e
+    int 10h
+    ret
+
 solve:
     xor si, si
     xor bx, bx
     
     lodsb
     mov bx, ax
-    
+
     lodsb
-    
-    cmp cl, 2
+
+    cmp cx, 2
     jne .not_dez
-    
-    cmp cl, 2
+
+    cmp cx, 2
     je .dez
 
     .not_dez:
@@ -77,7 +79,7 @@ solve:
         imul bx, 10
         add ax, bx
         ret
-         
+        
 fib:
     cmp cx, ax
     je .endfib
